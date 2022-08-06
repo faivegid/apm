2c8c49b35c7aa6c259952e372a22621e9c9a76cc
Gideon Faive <gfaive@axxess.com>
2022-08-06 11:32:43 +0100

    second commit

--- src/app/shared/components/star-component/star.component.ts
+++ src/app/shared/components/star-component/star.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
     selector: "pm-star",
-    templateUrl: "./star.component.html"
+    templateUrl: "./star.component.html",
+    styleUrls:["./star.component.css"]
 })
-export class StarComponent{
-    cropWidth: number = 0;
-    rating: number = 0;
+export class StarComponent implements OnChanges{
+    readonly starsWidth: number = 80;
+    cropWidth: number = 70;
+    @Input() rating: number = 4;
+
+    ngOnChanges(): void{
+        this.cropWidth = this.rating * 80/5
+    }
 }
